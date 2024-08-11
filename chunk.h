01f23692292b8d9b4639e1d8e4a1ebe9a2d72ea7
Nathan Boswell <nathan4044@gmail.com>
2024-08-11 13:05:25 +0100

    Add initial constant representation

--- chunk.h
+++ chunk.h
@@ -2,9 +2,11 @@
 #define lisp_chunk_h
 
 #include "common.h"
+#include "value.h"
 #include <stdint.h>
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -12,10 +14,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
