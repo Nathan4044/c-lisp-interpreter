d9c8977a9a41c4e8421dc375179d3d09d6b7cdd1
Nathan Boswell <nathan4044@gmail.com>
2024-11-29 16:58:06 +0000

    Add line information to Chunk
    line information is tracked 1-2-1 with bytecode instructions
    
    this information will be used for debugging information and error
    messages later on

--- chunk.c
+++ chunk.c
@@ -9,24 +9,30 @@ void initChunk(Chunk *chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
 // writeChunk writes a byte to the given Chunk.
-void writeChunk(Chunk *chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
-        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity,
+                chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity,
+                chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
 // freeChunk frees any allocated memory associated with a Chunk.
 void freeChunk(Chunk *chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
